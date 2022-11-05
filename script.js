d8260b44899f708e5ec921ccbf8894a9cdc8a45e
soithangsing <getdatasoi@outlook.com>
2022-11-05 14:23:30 +0530

    Finished desktop layout and basic mobile layout, need to add sidebar functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+
+function turnToX () {
+    // Make display none for the bars
+
+    // Append the X image to the Navbar?
+}
+mobileMenuButton.addEventListener('click', turnToX)
+
+// Strategy to display sidebar -> 
+
+// Create a new sidebar element that pops up when the menu button is pressed
